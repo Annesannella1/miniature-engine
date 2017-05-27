a91e895c1e6df418bd4db5aabe0bab4662c23737
Annesannella1 <annesannella@yahoo.com>
2017-05-27 09:06:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function MyApp()
+{
+    var version = "v1.0";
+
+    function setStatus(message)
+    {
+        $("#app>footer").text(message);
+    }
+    
+    this.start = function()
+    {
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
+}
+var myApp = {
+    version: "v1.0",
+    setStatus: function(message)
+    {
+        $("#app>footer").text(message);
+    },
+    start: function()
+    {
+        $("#app>header").append(this.version);
+        this.setStatus("ready");
+    };
+};
+$(document).ready(handler);
+$(function() {
+    window.app = new MyApp();
+    window.app.start();
+});
